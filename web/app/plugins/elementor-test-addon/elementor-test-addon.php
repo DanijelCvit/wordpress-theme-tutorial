4434fc1407f833bd9ac797ac4901647d4f35b991
Danijel Cvit <danijel.c+github@gmail.com>
2022-04-01 09:29:54 +0200

    added starter code for custom plugin

---
+++ web/app/plugins/elementor-test-addon/elementor-test-addon.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Elementor Test Addon
+ * Description: Custom Elementor addon.
+ * Plugin URI:  https://elementor.com/
+ * Version:     1.0.0
+ * Author:      DC
+ * Author URI:  https://developers.elementor.com/
+ * Text Domain: elementor-test-addon
+ *
+ * Elementor tested up to:     3.5.0
+ * Elementor Pro tested up to: 3.5.0
+ */
+
+function elementor_test_addon()
+{
+    //Load plugin file
+}
+
+add_action('plugins_loaded', 'elementor_test_addon');
